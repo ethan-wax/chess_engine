defa9e51be94d44e68c037d9245266cce50e3eb9
Ethan Waxman <ejw99@cornell.edu>
2025-11-23 12:54:29 -0500

    Complete Shannon score

--- backend/classical.py
+++ backend/classical.py
@@ -7,17 +7,19 @@ def classical_move(board: chess.Board):
 
 
 def shannon_score(board: chess.Board) -> int:
-    #     f(p) = 200(K-K')
-    #        + 9(Q-Q')
-    #        + 5(R-R')
-    #        + 3(B-B' + N-N')
-    #        + 1(P-P')
-    #        - 0.5(D-D' + S-S' + I-I')
-    #        + 0.1(M-M') + ...
+    """Calculate the Shannon score for the current position
+    f(p) = 200(K-K')
+           + 9(Q-Q')
+           + 5(R-R')
+           + 3(B-B' + N-N')
+           + 1(P-P')
+           - 0.5(D-D' + S-S' + I-I')
+           + 0.1(M-M') + ...
 
-    # KQRBNP = number of kings, queens, rooks, bishops, knights and pawns
-    # D,S,I = doubled, blocked and isolated pawns
-    # M = Mobility (the number of legal moves)
+    KQRBNP = number of kings, queens, rooks, bishops, knights and pawns
+    D,S,I = doubled, blocked and isolated pawns
+    M = Mobility (the number of legal moves)
+    """
     score = 0
     piece_count = count_pieces(board)
     score = 200 * (piece_count['K'] - piece_count['k']) \
@@ -28,6 +30,10 @@ def shannon_score(board: chess.Board) -> int:
         + 1 * (piece_count['P'] - piece_count['p']) 
     
     doubled = count_doubled_pawns(board)
+    stopped = count_stopped_pawns(board)
+    isolated = count_isolated_pawns
+    score -= 0.5 * (doubled[0] - doubled[1] + stopped[0] - stopped[1] + isolated[0] - isolated[1])
+    score += 0.1 * board.legal_moves.count()
     return score
 
 def count_pieces(board: chess.Board) -> dict[str, int]:
@@ -69,4 +75,29 @@ def count_stopped_pawns(board: chess.Board) -> tuple[int, int]:
                 square_below = (i - 1) * 8 + j
                 if board.piece_at(square_below):
                     black += 1
+    return white, black
+
+def count_isolated_pawns(board: chess.Board) -> tuple[int, int]:
+    pawns = [(False, False) for _ in range(8)]
+    for i in range(8):
+        for j in range(8):
+            square = i * 8 + j
+            piece = board.piece_at(square)
+            if piece and piece.symbol() == 'P':
+                pawns[j] = (True, pawns[j][1])
+            elif piece and piece.symbol() == 'p':
+                pawns[j] = (pawns[j][0], True)
+
+    white, black = 0, 0
+    for i in range(8):
+        if i == 0:
+            white += int(pawns[i][0] and not pawns[i+1][0])
+            black += int(pawns[i][1] and not pawns[i+1][1])
+        elif i == 7: 
+            white += int(pawns[i][0] and not pawns[i-1][0])
+            black += int(pawns[i][1] and not pawns[i-1][1])
+        else:
+            white += int(pawns[i][0] and (not pawns[i-1][0]) and (not pawns[i+1][0]))
+            white += int(pawns[i][1] and (not pawns[i-1][1]) and (not pawns[i+1][1]))
+    
     return white, black
--- backend/tests/test_classical.py
+++ backend/tests/test_classical.py
@@ -1,5 +1,5 @@
 import chess
-from classical import shannon_score, count_doubled_pawns, count_stopped_pawns
+from classical import count_isolated_pawns, shannon_score, count_doubled_pawns, count_stopped_pawns
 
 def test_shannon_score_initial_position():
     board = chess.Board()
@@ -48,3 +48,27 @@ def test_count_stopped_pawns():
     board.push_san('e5')
     stopped = count_stopped_pawns(board)
     assert stopped == (1, 1)
+
+def test_count_isolated_pawns_initial_position():
+    board = chess.Board()
+    isolated = count_isolated_pawns(board)
+    assert isolated == (0, 0)
+
+def test_count_isolated_pawns_clear_board():
+    board = chess.Board()
+    board.clear()
+    isolated = count_isolated_pawns(board)
+    assert isolated == (0, 0)
+
+def test_count_isolated_pawns():
+    board = chess.Board()
+    board.clear()
+    white_pawn = chess.Piece(chess.PAWN, chess.WHITE)
+    black_pawn = chess.Piece(chess.PAWN, chess.BLACK)
+    board.set_piece_at(chess.E3, white_pawn)
+    board.set_piece_at(chess.B3, white_pawn)
+    board.set_piece_at(chess.A3, white_pawn)
+    board.set_piece_at(chess.A5, black_pawn)
+    board.set_piece_at(chess.H5, black_pawn)
+    isolated = count_isolated_pawns(board)
+    assert isolated == (1, 2)
