2fc84e72d891e10f181a3509252bc1b9af251715
Ethan Waxman <ejw99@cornell.edu>
2025-11-23 13:21:18 -0500

    Simple negamax

--- backend/classical.py
+++ backend/classical.py
@@ -1,9 +1,52 @@
 import chess
 from collections import defaultdict
 
-def classical_move(board: chess.Board):
+
+def classical_move(
+    board: chess.Board, depth: int = 5, is_white: bool = False, alpha_beta: bool = False
+) -> str:
+    """Perform the negamax algorithm to select a move.
+
+    Uses the shannon score for evaluation.
+    """
+    if alpha_beta:
+        pass
+
+    max_score, best_move = -float("inf"), ""
     for move in board.legal_moves:
-        return board.san(move)
+        board.push(move)
+        score = nega_max(board, depth, is_white)
+        if score > max_score:
+            max_score = score
+            best_move = move
+        board.pop()
+
+    return board.san(best_move)
+
+
+def nega_max(board, depth, is_white) -> str:
+    if board.is_checkmate():
+        return (-1 if is_white else 1) * float("inf")
+
+    if (
+        board.is_stalemate()
+        or board.is_insufficient_material()
+        or board.is_seventyfive_moves()
+        or board.is_fivefold_repetition()
+    ):
+        return 0
+
+    if depth == 0:
+        return (1 if is_white else -1) * shannon_score(board)
+
+    max_score = -float("inf")
+    for move in board.legal_moves:
+        board.push(move)
+        score = -1 * nega_max(board, depth - 1, not is_white)
+        max_score = max(max_score, score)
+        board.pop(move)
+
+    return max_score
 
 
 def shannon_score(board: chess.Board) -> int:
@@ -14,27 +57,29 @@ def shannon_score(board: chess.Board) -> int:
            + 3(B-B' + N-N')
            + 1(P-P')
            - 0.5(D-D' + S-S' + I-I')
-           + 0.1(M-M') + ...
 
     KQRBNP = number of kings, queens, rooks, bishops, knights and pawns
     D,S,I = doubled, blocked and isolated pawns
-    M = Mobility (the number of legal moves)
     """
     score = 0
     piece_count = count_pieces(board)
-    score = 200 * (piece_count['K'] - piece_count['k']) \
-        + 9 * (piece_count['Q'] - piece_count['q']) \
-        + 5 * (piece_count['R'] - piece_count['r']) \
-        + 3 * (piece_count['B'] - piece_count['b']) \
-        + 3 * (piece_count['N'] - piece_count['n']) \
-        + 1 * (piece_count['P'] - piece_count['p']) 
-    
+    score = (
+        200 * (piece_count["K"] - piece_count["k"])
+        + 9 * (piece_count["Q"] - piece_count["q"])
+        + 5 * (piece_count["R"] - piece_count["r"])
+        + 3 * (piece_count["B"] - piece_count["b"])
+        + 3 * (piece_count["N"] - piece_count["n"])
+        + 1 * (piece_count["P"] - piece_count["p"])
+    )
+
     doubled = count_doubled_pawns(board)
     stopped = count_stopped_pawns(board)
-    isolated = count_isolated_pawns
-    score -= 0.5 * (doubled[0] - doubled[1] + stopped[0] - stopped[1] + isolated[0] - isolated[1])
-    score += 0.1 * board.legal_moves.count()
+    isolated = count_isolated_pawns(board)
+    score -= 0.5 * (
+        doubled[0] - doubled[1] + stopped[0] - stopped[1] + isolated[0] - isolated[1]
+    )
     return score
+
 
 def count_pieces(board: chess.Board) -> dict[str, int]:
     piece_count = defaultdict(int)
@@ -42,6 +87,7 @@ def count_pieces(board: chess.Board) -> dict[str, int]:
         p = piece.symbol()
         piece_count[p] = piece_count[p] + 1
     return piece_count
+
 
 def count_doubled_pawns(board: chess.Board) -> tuple[int, int]:
     white, black = 0, 0
@@ -51,9 +97,9 @@ def count_doubled_pawns(board: chess.Board) -> tuple[int, int]:
         for i in range(8):
             square = i * 8 + j
             piece = board.piece_at(square)
-            if piece and piece.symbol() == 'P':
+            if piece and piece.symbol() == "P":
                 white_col += 1
-            elif piece and piece.symbol() == 'p':
+            elif piece and piece.symbol() == "p":
                 black_col += 1
         if white_col >= 2:
             white += white_col
@@ -61,21 +107,23 @@ def count_doubled_pawns(board: chess.Board) -> tuple[int, int]:
             black += black_col
     return white, black
 
+
 def count_stopped_pawns(board: chess.Board) -> tuple[int, int]:
     white, black = 0, 0
     for i in range(8):
         for j in range(8):
             square = i * 8 + j
             piece = board.piece_at(square)
-            if piece and piece.symbol() == 'P':
+            if piece and piece.symbol() == "P":
                 square_above = (i + 1) * 8 + j
                 if board.piece_at(square_above):
                     white += 1
-            elif piece and piece.symbol() == 'p':
+            elif piece and piece.symbol() == "p":
                 square_below = (i - 1) * 8 + j
                 if board.piece_at(square_below):
                     black += 1
     return white, black
+
 
 def count_isolated_pawns(board: chess.Board) -> tuple[int, int]:
     pawns = [(False, False) for _ in range(8)]
@@ -83,21 +131,25 @@ def count_isolated_pawns(board: chess.Board) -> tuple[int, int]:
         for j in range(8):
             square = i * 8 + j
             piece = board.piece_at(square)
-            if piece and piece.symbol() == 'P':
+            if piece and piece.symbol() == "P":
                 pawns[j] = (True, pawns[j][1])
-            elif piece and piece.symbol() == 'p':
+            elif piece and piece.symbol() == "p":
                 pawns[j] = (pawns[j][0], True)
 
     white, black = 0, 0
     for i in range(8):
         if i == 0:
-            white += int(pawns[i][0] and not pawns[i+1][0])
-            black += int(pawns[i][1] and not pawns[i+1][1])
-        elif i == 7: 
-            white += int(pawns[i][0] and not pawns[i-1][0])
-            black += int(pawns[i][1] and not pawns[i-1][1])
+            white += int(pawns[i][0] and not pawns[i + 1][0])
+            black += int(pawns[i][1] and not pawns[i + 1][1])
+        elif i == 7:
+            white += int(pawns[i][0] and not pawns[i - 1][0])
+            black += int(pawns[i][1] and not pawns[i - 1][1])
         else:
-            white += int(pawns[i][0] and (not pawns[i-1][0]) and (not pawns[i+1][0]))
-            white += int(pawns[i][1] and (not pawns[i-1][1]) and (not pawns[i+1][1]))
-    
+            white += int(
+                pawns[i][0] and (not pawns[i - 1][0]) and (not pawns[i + 1][0])
+            )
+            white += int(
+                pawns[i][1] and (not pawns[i - 1][1]) and (not pawns[i + 1][1])
+            )
+
     return white, black
