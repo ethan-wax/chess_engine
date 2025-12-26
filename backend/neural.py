09ef6b6604ca60b1229702410caa55962ed92b4d
Ethan Waxman <ejw99@cornell.edu>
2025-12-26 12:51:44 -0500

    Fix probs

--- backend/neural.py
+++ backend/neural.py
@@ -2,8 +2,8 @@ import chess
 import numpy as np
 import torch
 
-from training.encoder import decode_move, encode_board, encode_move
-from training.simple_model import SimpleModel
+from .training.encoder import decode_move, encode_board, encode_move
+from .training.simple_model import SimpleModel
 
 device = "mps" if torch.backends.mps.is_available() else "cpu"
 model = SimpleModel().to(device)
@@ -16,7 +16,6 @@ def neural_move(board: chess.Board) -> str:
     board_enc = encode_board(board)
     board_enc = torch.from_numpy(board_enc).unsqueeze(0).to(device)
     _, model_moves = model(board_enc)
-    model_moves = model_moves.unsqueeze(0)
 
     legal_moves = np.zeros(4096, dtype=np.bool)
     for move in board.legal_moves:
@@ -25,7 +24,7 @@ def neural_move(board: chess.Board) -> str:
     legal_moves = torch.from_numpy(legal_moves).to(device)
 
     move_candidates = torch.where(legal_moves, model_moves, -float("inf"))
-    probs = torch.softmax(move_candidates, dim=0)
+    probs = torch.softmax(move_candidates, dim=-1)
     move = torch.multinomial(probs, 1).item()
 
     return board.san(decode_move(move))
